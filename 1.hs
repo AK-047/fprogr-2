45065332f2b8df3852e1081297f9a21bc0c63808
Nesk8er <pavkalashnikov@gmail.com>
2015-12-15 17:23:12 +0300

    adding to map

--- 1.hs
+++ 1.hs
@@ -1,43 +1,99 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-import System.Console.CmdArgs
-import Data.List.Split
-import Text.Regex
+import qualified Data.Matrix as M
+import qualified Data.Vector as V
+import System.Random
 import Data.Maybe
 import Control.Monad
 import Data.Conduit
 import Control.Monad.IO.Class
 import System.IO
+import Data.List.Split
+import qualified Data.Map.Strict as Map
 
 
+
+sumKey :: String
+sumKey = "SumKey"
+ 
+countKey :: String
+countKey = "CountKey"
+ 
+sumSqrKey :: String
+sumSqrKey = "SumSqrKey"
+
+main = do
+    let testPercent = 0.80
+    let test = []
+
+    source "butterfly.txt" $$ conduit test =$ sink
+
+    let dictionary = Map.fromList [("1", [(Map.fromList [(sumKey, 1.0), (countKey, 1.0), (sumSqrKey, 1.0)]), (Map.fromList [(sumKey, 1.0), (sumSqrKey, 1.0), (countKey, 1.0)])])]
+    let x = readX "2,3,1"
+    let g = updateClassAtrributes x dictionary
+    print g
+--    print dictionary    M.insertWith (++) "1" b dictionary
+
+--x = fromList [("1",                                [1,                                                        2, 3])]
+--m = fromList [("1",[(Map.fromList [(sumKey, 3.0), (countKey, 5.0), (sumSqrKey, 2.0)])])]         map for 2
+--updateClassAtrributes :: Map.Map String [Double] -> a -> a
+updateClassAtrributes x m = 
+    update
+    where
+        update = Map.insertWithKey (insertFunc) className xToMap m  
+        xToMap = map (\val -> (Map.fromList [(sumKey, val), (countKey, 1), (sumSqrKey, val**2)])) value
+        className = Map.keys x !! 0
+        value = x Map.! className
+        insertFunc k nV oV = zipWith (\ m v -> Map.adjust (+v) sumKey m) oV value -- oV,nv = List of maps
+        mapToMap map1 map2 = Map.mapWithKey (\key v -> Map.adjust (+v) key map1) map2 --map-map
+
+
+isForTest :: Float -> IO Bool
+isForTest percentage = do
+    a <- (randomIO :: IO Float)
+    return (a > percentage)
+
+
+teach a test percentage = isForTest percentage >>= (\val -> if val then return (((readX a):test,val)) else return ((test,val)))
+                                                                                        --teaching
+readX :: String -> Map.Map String [Double]
+readX x = Map.singleton (last splitted) (readFloat $ init splitted)
+    where
+        splitted = splitOn "," x
+
+
+
+readFloat :: [String] -> [Double]
+readFloat = map read
 
 source path = do
-	file <- liftIO $ openFile path ReadMode
-	readToEnd file
-	where
-		readToEnd file = do
-			eof <- liftIO $ hIsEOF file
-			if eof
-				then return ()
-				else do
-					line <- liftIO $ hGetLine file
-					yield line
-					readToEnd file
-		
-conduit = do
+    file <- liftIO $ openFile path ReadMode
+    readToEnd file
+    where
+        readToEnd file = do
+            eof <- liftIO $ hIsEOF file
+            if eof
+                then return ()
+                else do
+                    line <- liftIO $ hGetLine file
+                    yield line
+                    readToEnd file
+
+conduit testVectors = do
     mx <- await
     case mx of
-        Nothing -> return ()
+        Nothing -> do
+        	--testing
+        	--testing
+        	--testing
+        	return ()
         Just x -> do
+            (testVectors, wasAdded) <- liftIO $ teach x testVectors 0.8
+            --liftIO $ print $ testVectors
             yield x
-            conduit
+            conduit testVectors
 
 sink = do
     mx <- await
     case mx of
         Nothing -> return ()
         Just x -> do
-            liftIO $ putStrLn $ x
             sink
-
-main = do
-	source "butterfly.txt" $$ conduit =$ sink
--- main.hs
+++ main.hs
@@ -1,20 +1,46 @@
-import Data.Matrix as M
-import Data.Vector as V
+import qualified Data.Matrix as M
+import qualified Data.Vector as V
 import System.Random
 import Data.Maybe
 import Control.Monad
 import Data.Conduit
 import Control.Monad.IO.Class
 import System.IO
+import Data.List.Split
+import qualified Data.Map.Strict as Map
 
 
 
+sumKey :: String
+sumKey = "SumKey"
+ 
+countKey :: String
+countKey = "CountKey"
+ 
+sumSqrKey :: String
+sumSqrKey = "SumSqrKey"
 
 main = do
     let testPercent = 0.80
     let test = []
-    source "butterfly.txt" $$ conduit test =$ sink
-    
+    let dictionary = Map.fromList []
+    source "1.txt" $$ conduit test dictionary =$ sink
+
+updateClassAtrributes x m = 
+    update
+    where
+        update = Map.insertWithKey (insertFunc) className xToMap m  
+        xToMap = map (\val -> (Map.fromList [(sumKey, val), (countKey, 1), (sumSqrKey, val**2)])) value
+        className = Map.keys x !! 0
+        value = x Map.! className
+        insertFunc k nV oV = zipWith (\ m v -> updateWithKey m v [sumKey, countKey, sumSqrKey]) oV value -- oV,nv = List of maps
+
+updateWithKey a v [] = a
+updateWithKey a v (x:xs) 
+    | x == sumKey = updateWithKey (Map.adjust (+v) x a) v xs
+    | x == countKey = updateWithKey (Map.adjust (+1) x a) v xs
+    | x == sumSqrKey = updateWithKey (Map.adjust (+v**2) x a) v xs
+
 
 isForTest :: Float -> IO Bool
 isForTest percentage = do
@@ -22,8 +48,18 @@ isForTest percentage = do
     return (a > percentage)
 
 
-teach a test percentage = isForTest percentage >>= (\val -> if val then return ((a:test,val)) else return ((test,val)))
-                                                                                             --teaching
+teach a test percentage = isForTest percentage >>= (\val -> if val then return (((readX a):test,val)) else return ((test,val)))
+                                                                                        --teaching
+readX :: String -> Map.Map String [Double]
+readX x = Map.singleton (last splitted) (readFloat $ init splitted)
+    where
+        splitted = splitOn "," x
+
+
+
+readFloat :: [String] -> [Double]
+readFloat = map read
+
 source path = do
     file <- liftIO $ openFile path ReadMode
     readToEnd file
@@ -36,26 +72,26 @@ source path = do
                     line <- liftIO $ hGetLine file
                     yield line
                     readToEnd file
-        
-conduit testVectors = do
+
+conduit testVectors dict = do
     mx <- await
     case mx of
         Nothing -> do
         	--testing
         	--testing
         	--testing
-        	return ()
+            yield dict
         Just x -> do
-            (testVectors, wasAdded) <- liftIO $ teach x testVectors 0.8
-            liftIO $ print $ testVectors
-            liftIO $ print $ wasAdded
-            yield x
-            conduit testVectors
+            (testVectors, wasAdded) <- liftIO $ teach x testVectors 0.5
+            --liftIO $ print $ testVectors
+            --yield x
+            if wasAdded then conduit testVectors dict else conduit testVectors $ updateClassAtrributes (readX x) dict
 
 sink = do
     mx <- await
     case mx of
         Nothing -> return ()
         Just x -> do
-            liftIO $ putStrLn $ x
+            --liftIO $ print $ Map.showTreeWith (\k x -> show (k,x)) False True x
+            liftIO $ print $ x
             sink
